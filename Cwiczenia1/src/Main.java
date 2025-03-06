c27acbae96464b68edb044bcfc937d18acaf0515
Michal Pazio <elmiciu@gmail.com>
2025-03-06 09:37:52 +0100

    Change Main.java

--- Cwiczenia1/src/Main.java
+++ Cwiczenia1/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 10;
+        int b = 11;
+        int c = 12;
     }
 }
